de854bb1d745034d156ea62198a7726459bb4ec7
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-02-17 10:18:04 +0100

    fix: project config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    
-    "./src/app/Components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        dark: "#0a0a0a",
+        light: "#f7f8f8",
+        content: "#8a8f98",
+        gold: "#FFD700"
       },
+      keyframes: {
+        fadeSlideIn: {
+          "0%": { opacity: "0", transform: "translateY(50px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeSlideIn: "fadeSlideIn 1s ease-out forwards",
+      },
+      container: {
+        center: true,
+        padding: "24px",
+      }
     },
   },
   plugins: [],
-};
-
-export default config;
+} satisfies Config;
