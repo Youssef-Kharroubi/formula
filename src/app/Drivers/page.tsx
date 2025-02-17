0db10b11ad0048352356cafb65a11afa30873105
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-02-17 10:21:42 +0100

    add: driver listing page with it's child component

--- src/app/Drivers/page.tsx
+++ src/app/Drivers/page.tsx
@@ -1,64 +1,77 @@
 'use client'
-import DriverCard from "../Components/DriverCard";
-import axios from "axios";
-import { parseString } from 'xml2js';
-import {
-    useEffect,
-    useState
-} from "react";
+import DriverCard from "../../components/ui/DriverCard";
 import Image from "next/image";
+import {useState,useEffect} from 'react';
+import DriverCardSkeleton from "@/components/ui/DriverCardSkeleton";
 
-class Driver{
-    driverId:string;
-    GivenName:string;
-    FamilyName:string;
-    DateOfBirth:string;
-    PermanentNumber:string;
-    Nationality:string;
+export interface DriverDTO{
+    boradcast_name: string;
+    country_code: string;
+    driver_number: string;
+    first_name: string;
+    last_name: string;
+    headshot_url: string;
+    permanentNumber: string;
+    team_name: string;
+    team_colour: string,
+    meeting_key: string,
+    session_key: string,
 }
+const API_ENDPOINT = 'https://api.openf1.org/v1/drivers';
 
 export default function Drivers() {
-    const [Drivers,setDrivers] = useState<Driver[]>([]);
+    const [Drivers, setDrivers] = useState<DriverDTO[]>([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-        axios.get('http://ergast.com/api/f1/2024/drivers')
-            .then((response) => {
-                console.log(response.data);
-                parseString(response.data, { explicitArray: false }, (err, result) => {
-                    if (err) {
-                        console.error("Error parsing XML:", err);
-                        return;
-                    }
-                    const drivers = result.MRData.DriverTable.Driver || [];
-                    setDrivers(drivers);
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+        const fetchDrivers = async() => {
+        try{
+        const data = await fetch(API_ENDPOINT).then(response => response.json())
+            const driversTable = (data|| []).filter((driver:DriverDTO)=> driver.session_key == "9158");
+            setDrivers(driversTable);
+        } catch(error) {
+                console.error("Error Fetching Data",error);
+            }
+        finally {
+                setLoading(false);
+            }
+
+    };
+        fetchDrivers();},[]);
+
     return (
-        <>
+        <section className="container">
             <h1 className="text-4xl md:text-6xl font-extrabold text-red-600 text-center leading-tight m-10">
                 F1 Drivers LineUp
                 for 2025!</h1>
             <div className="flex justify-center items-center mt-5 ">
-                <Image src="/lineup2025.jpg"
+                <Image src="/images/lineup2025.jpg"
                        alt="carousel"
-                       priority
                        width={700}
                        height={300}
+                       priority
+                       loading="eager"
                        className="object-cover w-2/3 h-auto rounded-md "></Image>
             </div>
-            <div className="flex justify-center items-center w-screen">
-            <hr className="mt-10 w-5/6  border-t-3 border-black opacity-100"/>
-            </div>
-                <div className="flex justify-center items-center m-2 w-screen">
-                <div className="flex flex-wrap justify-center items-center  ">
-                    {Array.isArray(Drivers) && Drivers.map((Driver, index) => (
-                        <DriverCard key={index} Driver={Driver}/>
-                    ))}</div>
-            </div>
+            <span className="block h-px border border-b-light/20 w-[calc(100%-64px)] mx-auto my-12"/>
+            {loading || Drivers.length === 0 ? (
+                <div className="grid lg:grid-cols-4 grid-cols-1 lg:gap-3 gap-2">
+                    {Array.from({ length: 20 }).map((_, index) => (
+                        <DriverCardSkeleton key={index} />
+                    ))}
+                </div>
+            ) : (
+                Drivers.length > 0 && (
+                    <div className="grid lg:grid-cols-4 grid-cols-1 lg:gap-3 gap-2">
+                        {Drivers.map((driver, index) => (
+                            <DriverCard key={index} driver={driver} />
+                        ))}
+                    </div>
+                )
+            )}
 
-        </>
+        </section>
+
+
     );
 }
--- src/components/ui/DriverCard.tsx
+++ src/components/ui/DriverCard.tsx
@@ -1,35 +1,45 @@
+import {DriverDTO} from "@/app/Drivers/page"
+import Link from "next/link";
+import Image from "next/image";
 
+interface Props{
+    driver: DriverDTO;
+}
+export default function DriverCard({driver}: Readonly<Props>){
 
-export default function DriverCard({Driver}){
-    function Age(DateString){
-        const today = new Date();
-        const birthDate = new Date(DateString);
-        let age = today.getFullYear() - birthDate.getFullYear();
-        const month = today.getMonth() - birthDate.getMonth();
-        if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
-        return age;
-    }
     return(
-        <>
+        <Link href="/" className="border rounded-lg p-4">
 
-            <div className="border-2 border-red-600 rounded-lg m-3 mb-5 p-1 w-1/4 transition-all duration-300 hover:scale-105">
-                <div className="grid grid-cols-2 items-center justify-center mb-5">
-                    <p className="text-center mt-2"><b>Name</b>: {Driver.GivenName}</p>
-                    <p className="text-center mt-2"><b>LastName</b>: {Driver.FamilyName}</p>
+            <div className="grid grid-cols-2 items-center gap-4">
+                <div className="flex flex-col">
+                    <span className="text-3xl font-bold text-light">{driver.driver_number}</span>
+                    <div className="mt-8 flex flex-col ">
+                        <span className="text-2xl text-light/70">{driver.first_name}</span>
+                        <span className="text-3xl text-light font-bold">{driver.last_name}</span>
+                    </div>
                 </div>
-                <div className="grid grid-cols-3 items-center justify-center">
-                    <p className="text-center mt-2">{Driver.Nationality}</p>
-                    <p className="text-center mt-2"> <b>N°</b>{Driver.PermanentNumber}</p>
-                    <p className="text-center mt-2"><b>Age</b>: {Age(Driver.DateOfBirth)}</p>
-                </div>
-                <div className="flex justify-start items-start">
-                    <a href={Driver.$.url}
-                       className="m-3 p-1  hover:bg-black hover:text-white border-gray-700   ring-1 rounded-lg flex items-center justify-center w-2/6">Go
-                        To Wikipedia</a>
+                <div className="flex justify-end">
+                    <Image
+                        src={driver.headshot_url || "/images/lineup2025.jpg"}
+                        alt="Headshot"
+                        width={220}
+                        height={220}
+                        className="object-cover rounded-4xl  "
+                    />
                 </div>
             </div>
-        </>
+
+            {/*<div className="border-2 border-red-600 rounded-lg m-3 mb-5 p-1 w-1/4 ">*/}
+            {/*    <div className="grid grid-cols-2 items-center justify-center mb-5">*/}
+            {/*        <p className="text-center mt-2"><b>Name</b>: {driver.givenName}</p>*/}
+            {/*        <p className="text-center mt-2"><b>LastName</b>: {driver.familyName}</p>*/}
+            {/*    </div>*/}
+            {/*    <div className="grid grid-cols-3 items-center justify-center">*/}
+            {/*        <p className="text-center mt-2">{driver.nationality}</p>*/}
+            {/*        <p className="text-center mt-2"> <b>N°</b>{driver.permanentNumber}</p>*/}
+            {/*        <p className="text-center mt-2"><b>Age</b>: {driver.dateOfBirth}</p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </Link>
     );
 }
