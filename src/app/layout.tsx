99097480856497d275480ecb7b470fe0e47cea31
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-02-17 10:19:33 +0100

    fix: choose app architecture + enhancing the overall layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import Layout from "./Components/Layout";
-
+import "./globals.css";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,10 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-      <Layout>{children}</Layout>
+      <Header/>
+      <main>{children}</main>
+      <Footer/>
       </body>
     </html>
   );
